c102f9256e107eb5deb08b400667c8a5e0f4ef97
MarkFCain <mark.cain@digital.education.gov.uk>
2020-04-22 15:34:28 +0100

    APR-1607 locking down the roles to be AOV only

--- src/SFA.DAS.RoatpOversight/SFA.DAS.RoatpOversight.Web/Domain/Roles.cs
+++ src/SFA.DAS.RoatpOversight/SFA.DAS.RoatpOversight.Web/Domain/Roles.cs
@@ -6,19 +6,11 @@ namespace SFA.DAS.RoatpOversight.Web.Domain
     {
         public const string RoleClaimType = "http://service/service";
 
-        public const string RoatpGatewayTeam = "APR";
-        public const string RoatpGatewayAssessorTeam = "GAC";
-        public const string RoatpFinancialAssessorTeam = "FHC"; // RoATP FHA
         public const string RoatpApplicationOversightTeam = "AOV";
-        public const string RoatpOversightTeam = "AAC";
 
         public static bool HasValidRole(this ClaimsPrincipal user)
         {
-            return user.IsInRole(RoatpGatewayTeam)
-                   || user.IsInRole(RoatpGatewayAssessorTeam)
-                   || user.IsInRole(RoatpFinancialAssessorTeam)
-                   || user.IsInRole(RoatpOversightTeam)
-                   || user.IsInRole(RoatpApplicationOversightTeam);
+            return user.IsInRole(RoatpApplicationOversightTeam);
         }
     }
 }

c1294fec78fad32c1e3316bea8142cc180c617ca
chrisfoster186 <chrisfoster186@googlemail.com>
2021-01-19 17:23:53 +0000

    Adds unit test for new statuses

--- src/SFA.DAS.RoatpOversight.Web.UnitTests/Services/ApplicationOutcomeOrchestratorTests.cs
+++ src/SFA.DAS.RoatpOversight.Web.UnitTests/Services/ApplicationOutcomeOrchestratorTests.cs
@@ -76,5 +76,41 @@ namespace SFA.DAS.RoatpOversight.Web.UnitTests.Services
             _applicationApiClient.Verify(x => x.RecordOutcome(It.Is<RecordOversightOutcomeCommand>(y => y.ApplicationId == _applicationId)), Times.Once);
             _roatpRegisterApiClient.Verify(x => x.CreateOrganisation(It.IsAny<CreateRoatpOrganisationRequest>()), Times.Never);
         }
+
+        [TestCase(OversightReviewStatus.SuccessfulAlreadyActive)]
+        [TestCase(OversightReviewStatus.SuccessfulFitnessForFunding)]
+        public async Task Application_status_and_register_updated_for_a_successful_already_active_or_fitness_for_funding_oversight_review(string status)
+        {
+            var organisationId = Guid.NewGuid();
+
+            _applicationApiClient.Setup(x => x.RecordOutcome(It.Is<RecordOversightOutcomeCommand>(y => y.ApplicationId == _applicationId))).ReturnsAsync(true);
+
+            var registrationDetails = new RoatpRegistrationDetails
+            {
+                CharityNumber = "112233",
+                CompanyNumber = "1122111",
+                LegalName = "Legal Name",
+                OrganisationTypeId = 2,
+                ProviderTypeId = 1,
+                TradingName = "Trading as name",
+                UKPRN = "19902000"
+            };
+
+            _applicationApiClient.Setup(x => x.GetRegistrationDetails(_applicationId)).ReturnsAsync(registrationDetails);
+
+            _roatpRegisterApiClient.Setup(x =>
+                    x.GetOrganisationRegisterStatus(
+                        It.Is<GetOrganisationRegisterStatusRequest>(r => r.UKPRN == registrationDetails.UKPRN)))
+                .ReturnsAsync(() => new OrganisationRegisterStatus{OrganisationId = organisationId});
+
+            _roatpRegisterApiClient.Setup(x => x.CreateOrganisation(It.Is<CreateRoatpOrganisationRequest>(y => y.Ukprn == registrationDetails.UKPRN))).ReturnsAsync(true);
+
+            _orchestrator = new ApplicationOutcomeOrchestrator(_applicationApiClient.Object, _roatpRegisterApiClient.Object, _logger.Object);
+
+            await _orchestrator.RecordOutcome(_applicationId, status, UserId, UserName);
+
+            _applicationApiClient.Verify(x => x.RecordOutcome(It.Is<RecordOversightOutcomeCommand>(y => y.ApplicationId == _applicationId)), Times.Once);
+            _roatpRegisterApiClient.Verify(x => x.UpdateApplicationDeterminedDate(It.Is<UpdateOrganisationApplicationDeterminedDateRequest>(y => y.OrganisationId == organisationId)), Times.Once);
+        }
     }
 }

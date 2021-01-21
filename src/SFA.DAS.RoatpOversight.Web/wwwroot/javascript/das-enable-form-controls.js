f31cff2048ecd847dbf17a1f8d214832f200aa86
gdbarnes <gregdbarnes@gmail.com>
2021-01-21 14:04:43 +0000

    Add markup, fix tag classes, fix js on browser back

--- src/SFA.DAS.RoatpOversight.Web/wwwroot/javascript/das-enable-form-controls.js
+++ src/SFA.DAS.RoatpOversight.Web/wwwroot/javascript/das-enable-form-controls.js
@@ -2,36 +2,48 @@ var dasJs = dasJs || {};
 
 dasJs.enableFormControlsOnChange = {
   init: function (radioGroupsRequiredToEnable) {
+
     if (!document.querySelectorAll('input[data-disabled]').length) return false;
+
     var disabledInputs = document.querySelectorAll('input[data-disabled]');
     this.handleDisabledInputs(radioGroupsRequiredToEnable, disabledInputs)
   },
 
   handleDisabledInputs: function (radioGroupsRequiredToEnable, disabledInputs) {
-    function allRequiredRadiosChecked() {
-      for (var i = 0; i < radioGroupsRequiredToEnable.length; i++) {
-        if (!document.querySelector('input[name="' + radioGroupsRequiredToEnable[i] + '"]:checked')) return false;
+    var radioGroupsStillRequired = [];
+
+    function noRadiosInGroupChecked(groupName) {
+      var radiosInGroupNodeList = document.querySelectorAll('input[name="' + groupName + '"]');
+      for (var j = 0; j < radiosInGroupNodeList.length; j++) {
+        if (radiosInGroupNodeList[j].checked) return false;
       }
       return true;
     }
 
-    if (!allRequiredRadiosChecked()) {
+    for (var i = 0; i < radioGroupsRequiredToEnable.length; i++) {
+      if (noRadiosInGroupChecked(radioGroupsRequiredToEnable[i])) {
+        radioGroupsStillRequired.push(radioGroupsRequiredToEnable[i])
+      }
+    }
+
+    if (radioGroupsStillRequired.length) {
       for (var i = 0; i < disabledInputs.length; i++) {
         disabledInputs[i].disabled = true
       }
-      this.handleFormControls(radioGroupsRequiredToEnable, disabledInputs);
+
+      this.handleFormControls(radioGroupsStillRequired, disabledInputs);
     }
   },
 
-  handleFormControls: function (radioGroupsRequiredToEnable, disabledInputs) {
+  handleFormControls: function (radioGroupsStillRequired, disabledInputs) {
     document.addEventListener("click", function (event) {
       if (event.target.name === undefined) return false;
 
-      for (var i = 0; i < radioGroupsRequiredToEnable.length; i++) {
-        if (event.target.name === radioGroupsRequiredToEnable[i]) {
-          radioGroupsRequiredToEnable.splice(radioGroupsRequiredToEnable.indexOf(event.target.name), 1);
+      for (var i = 0; i < radioGroupsStillRequired.length; i++) {
+        if (event.target.name === radioGroupsStillRequired[i]) {
+          radioGroupsStillRequired.splice(radioGroupsStillRequired.indexOf(event.target.name), 1);
 
-          if (radioGroupsRequiredToEnable.length < 1 || radioGroupsRequiredToEnable === undefined) {
+          if (radioGroupsStillRequired.length < 1 || radioGroupsStillRequired === undefined) {
             for (var i = 0; i < disabledInputs.length; i++) {
               disabledInputs[i].disabled = false
             }

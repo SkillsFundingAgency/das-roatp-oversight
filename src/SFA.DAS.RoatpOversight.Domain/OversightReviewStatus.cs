d7982167f9dae9907fae40ba26f5a78f1f5a318e
chrisfoster186 <chrisfoster186@googlemail.com>
2021-01-21 15:12:04 +0000

    Adds status field to table on confirmation

--- src/SFA.DAS.RoatpOversight.Domain/OversightReviewStatus.cs
+++ src/SFA.DAS.RoatpOversight.Domain/OversightReviewStatus.cs
@@ -4,12 +4,11 @@ namespace SFA.DAS.RoatpOversight.Domain
 {
     public static class OversightReviewStatus
     {
-        public const string New = "New";
-        public const string InProgress = "InProgress";
+        public const string InProgress = "In progress";
         public const string Successful = "Successful";
         public const string Unsuccessful = "Unsuccessful";
-        public const string SuccessfulAlreadyActive = "SuccessfulAlreadyActive";
-        public const string SuccessfulFitnessForFunding = "SuccessfulFitnessForFunding";
+        public const string SuccessfulAlreadyActive = "Successful - already active";
+        public const string SuccessfulFitnessForFunding = "Successful - fitness for funding";
 
         public static IReadOnlyList<string> SuccessfulStatuses { get; } = new List<string>
         {

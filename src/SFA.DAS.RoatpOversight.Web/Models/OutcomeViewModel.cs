db99eec3305c1c3f1ed97fd6960cacbef46e969a
chrisfoster186 <chrisfoster186@googlemail.com>
2021-03-17 13:38:20 +0000

    Tidy up

--- src/SFA.DAS.RoatpOversight.Web/Models/OutcomeViewModel.cs
+++ src/SFA.DAS.RoatpOversight.Web/Models/OutcomeViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using SFA.DAS.ApplyService.Types;
-using SFA.DAS.RoatpOversight.Domain;
 using SFA.DAS.RoatpOversight.Web.Models.Partials;
 
 namespace SFA.DAS.RoatpOversight.Web.Models
@@ -26,21 +25,18 @@ namespace SFA.DAS.RoatpOversight.Web.Models
         public string ApproveGateway { get; set; }
         public string ApproveModeration { get; set; }
 
-        public string SuccessfulText { get; set; }  
+        public string SuccessfulText { get; set; }
+        public string SuccessfulExternalText { get; set; }
 
         public string SuccessfulAlreadyActiveText { get; set; }
+        public string SuccessfulAlreadyActiveExternalText { get; set; }
         public string SuccessfulFitnessForFundingText { get; set; }
         public string UnsuccessfulText { get; set; }
         public string UnsuccessfulExternalText { get; set; }
         public string InProgressInternalText { get; set; }
         public string InProgressExternalText { get; set; }
         public OversightReviewStatus OversightStatus { get; set; }
-        
         public AppealStatus SelectedAppealStatus { get; set; }
-        
-        public string SuccessfulExternalText { get; set; }
-
-        public string SuccessfulAlreadyActiveExternalText { get; set; }
         public string UnsuccessfulPartiallyUpheldText { get; set; }
         public string UnsuccessfulPartiallyUpheldExternalText { get; set; }
     }

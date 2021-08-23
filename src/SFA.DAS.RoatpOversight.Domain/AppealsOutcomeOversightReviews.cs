fd6dc4320034b2118e28fc48adffcf792cc15bdb
SureshKuruva1 <Suresh.Kuruva11@yahoo.co.uk>
2021-08-23 10:03:54 +0100

    apr-2624 changes

---
+++ src/SFA.DAS.RoatpOversight.Domain/AppealsOutcomeOversightReviews.cs
@@ -0,0 +1,26 @@
+﻿using SFA.DAS.ApplyService.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.RoatpOversight.Domain
+{
+    public class AppealsOutcomeOversightReviews
+    {
+        public List<AppealsOutcomeOversightReview> Reviews { get; set; }
+    }
+
+    public class AppealsOutcomeOversightReview
+    {
+        public Guid ApplicationId { get; set; }
+        public string OrganisationName { get; set; }
+        public string Ukprn { get; set; }
+        public string ProviderRoute { get; set; }
+        public string ApplicationReferenceNumber { get; set; }
+        public DateTime? AppealSubmittedDate { get; set; }
+        public DateTime? AppealDeterminedDate { get; set; }
+        public string ApplicationStatus { get; set; }
+        public string AppealOutcome { get; set; }
+
+    }
+}
--- src/SFA.DAS.RoatpOversight.Web/Models/ApplicationsViewModel.cs
+++ src/SFA.DAS.RoatpOversight.Web/Models/ApplicationsViewModel.cs
@@ -8,6 +8,10 @@ namespace SFA.DAS.RoatpOversight.Web.Models
         public int ApplicationCount { get; set; }
         public CompletedOversightReviews OverallOutcomeDetails { get; set; }
         public int OverallOutcomeCount { get; set; }
+        public AppealsOversightReviews AppealsDetails { get; set; }
+        public int AppealsCount { get; set; }
+        public AppealsOutcomeOversightReviews AppealsOutcomeDetails { get; set; }
+        public int AppealsOutcomeCount { get; set; }
 
         public string SearchTerm { get; set; }
         public string SortColumn { get; set; }

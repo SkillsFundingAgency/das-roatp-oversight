202264c05bbd4c3ccb2092ff0f4c04c0657da3c0
MarkFCain <mark.cain@digital.education.gov.uk>
2021-08-31 11:37:03 +0100

    APR-2628 coverage complete

--- src/SFA.DAS.RoatpOversight.Web/Models/AppealPostRequest.cs
+++ src/SFA.DAS.RoatpOversight.Web/Models/AppealPostRequest.cs
@@ -8,21 +8,12 @@ namespace SFA.DAS.RoatpOversight.Web.Models
         public Guid ApplicationId { get; set; }
 
         public string AppealStatus { get; set; }
-        //   public string ApproveGateway { get; set; }
-        //   public string ApproveModeration { get; set; }
         public string SuccessfulText { get; set; }
-        // public string SuccessfulExternalText { get; set; }
         public string SuccessfulAlreadyActiveText { get; set; }
-        //    public string SuccessfulAlreadyActiveExternalText { get; set; }
         public string SuccessfulFitnessForFundingText { get; set; }
         public string UnsuccessfulText { get; set; }
         public string UnsuccessfulExternalText { get; set; }
         public string InProgressInternalText { get; set; }
         public string InProgressExternalText { get; set; }
-        //   public bool IsGatewayFail { get; set; }
-        //   public bool IsGatewayRemoved { get; set; }
-        //    public string UnsuccessfulPartiallyUpheldText { get; set; }
-        //    public string UnsuccessfulPartiallyUpheldExternalText { get; set; }
-
     }
 }

65981ba15da012dc75b9e7f008d3d145ff95f4a2
SureshKuruva1 <Suresh.Kuruva11@yahoo.co.uk>
2021-09-16 07:36:23 +0100

    1) Update inprogress comments 2) Display Application Outcome section when outcome status is Successful already active and successfull fitness

---
+++ src/SFA.DAS.RoatpOversight.Web/Extensions/AppealStatusExtensions.cs
@@ -0,0 +1,48 @@
+﻿using SFA.DAS.RoatpOversight.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.RoatpOversight.Web.Extensions
+{
+    public static class AppealStatusExtensions
+    {
+        public static string GetLabel(this string status)
+        {
+            switch (status)
+            {
+                case AppealStatus.Successful:
+                case AppealStatus.SuccessfulAlreadyActive:
+                case AppealStatus.SuccessfulFitnessForFunding:
+                    return "Successful";
+                case AppealStatus.InProgress:
+                    return "In progress";
+                case AppealStatus.Unsuccessful:
+                    return "Unsuccessful";
+                case AppealStatus.Submitted:
+                    return "Submitted";
+                default:
+                    return "";
+            }
+        }       
+        public static string GetCssClass(this string status)
+        {
+            switch (status)
+            {
+                case AppealStatus.Successful:
+                case AppealStatus.SuccessfulAlreadyActive:
+                case AppealStatus.SuccessfulFitnessForFunding:
+                    return "govuk-tag das-tag--solid-green";
+                case AppealStatus.Unsuccessful:
+                    return "govuk-tag das-tag--solid-red";
+                case AppealStatus.InProgress:
+                case AppealStatus.Submitted:
+                    return "govuk-tag das-tag";
+                default:
+                    return "";
+            }
+        }
+
+    }
+}

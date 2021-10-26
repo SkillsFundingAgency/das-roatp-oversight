773c1c848155535339fd950e3ce6742b03627a0a
MarkFCain <mark.cain@digital.education.gov.uk>
2021-10-26 09:24:01 +0100

    APR-2737 code added

--- src/SFA.DAS.RoatpOversight.Web/Services/ApplicationOutcomeOrchestrator.cs
+++ src/SFA.DAS.RoatpOversight.Web/Services/ApplicationOutcomeOrchestrator.cs
@@ -55,17 +55,22 @@ namespace SFA.DAS.RoatpOversight.Web.Services
             if ((outcome == OversightReviewStatus.SuccessfulAlreadyActive ||
                  outcome == OversightReviewStatus.SuccessfulFitnessForFunding) && registerStatus.OrganisationId != null)
             {
-                var updateDeterminedDateRequest = new UpdateOrganisationApplicationDeterminedDateRequest
+                var updateOrganisationRequest = new UpdateOrganisationRequest
                 {
                     ApplicationDeterminedDate = DateTime.UtcNow.Date,
                     LegalName = registrationDetails.LegalName,
                     OrganisationId = registerStatus.OrganisationId.Value,
-                    UpdatedBy = userId
+                    Username = userName,
+                    CharityNumber = registrationDetails.CharityNumber,
+                    CompanyNumber = registrationDetails.CompanyNumber,
+                    OrganisationTypeId = registrationDetails.OrganisationTypeId,
+                    ProviderTypeId = registrationDetails.ProviderTypeId,
+                    TradingName = registrationDetails.TradingName,
                 };
 
-                await _registerApiClient.UpdateApplicationDeterminedDate(updateDeterminedDateRequest);
+                return await _registerApiClient.UpdateOrganisation(updateOrganisationRequest);
             }
-            
+
             return true;
         }
 

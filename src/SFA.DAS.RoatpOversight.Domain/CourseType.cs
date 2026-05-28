17b50c8d6447f0022d5a24a2dc50ee8a3e2a1d24
Deven Shah <devenshah1974@gmail.com>
2026-05-28 09:40:00 +0100

    CSP-2610 Allows Apprenticeship course type to newly approved providers (#102)

---
+++ src/SFA.DAS.RoatpOversight.Domain/CourseType.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.RoatpOversight.Domain;
+
+public static class CourseType
+{
+    public const int Apprenticeship = 1;
+    public const int ShortCourse = 2;
+}
---
+++ src/SFA.DAS.RoatpOversight.Domain/UpdateCourseTypesRequest.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.RoatpOversight.Domain;
+
+public record UpdateCourseTypesRequest(IEnumerable<int> CourseTypeIds, string UserId);
